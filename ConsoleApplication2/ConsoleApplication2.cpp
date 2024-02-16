3e428fe8705bf242512dd323a3ad310671bf0b02
myroniukkk <myroniukkk@DESKTOP-33BST9I>
2024-02-16 12:01:20 +0300

    ввела фрагмент, який запитує ввести розмір масиву, зчитує введене значення та зберігає його у змінній n, дописала функцію

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,20 +1,30 @@
-// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <locale>
+#include <conio.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+void task1() {
+
+    int N;
+    cout << "Input size of array N: ";
+    cin >> N;
+    int A[100];
+    cout << "Input element of array A:" << endl;
+        for (int i = 0; i < N; ++i) {
+            cout << "A[" << i << "]: ";
+            cin >> A[i];
+        }
+    long long C[100];
+    for (int i = 0; i < N; ++i) {
+        C[i] = static_cast<long long>(pow(2, i + 1)) * A[i];
+    }
+    cout << "Array C:" << endl;
+    for (int i = 0; i < N; ++i) {
+        cout << "C[" << i << "]: " << C[i] << endl;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
